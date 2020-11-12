0d59616ed3f092f330f6bd97ead9f12eabf6a3c7
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-11-12 15:27:14 -0500

    updated formatting, added humor

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// || Require dependencies 
 const express = require("express");
 
+// || Assign dependencies 
 const app = express();
-
 app.use(express.static("public"));
 
+// || Routes 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
@@ -13,10 +15,10 @@ app.get("/", function(req, res) {
 
 
 
-
-
+// || Server listener compatible with Heroku, Localhost, and A2
 let port = process.env.PORT;
-if (port == null || port == "") { 
+if (port === null || port === "") { 
     port = 3000; 
 };
 app.listen(port, () => console.log(`Server started at port ${port}.`));
+// || END Listener 
