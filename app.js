ac850f0fcddb8982c0c0483cef90ed6ef003901b
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2020-11-12 15:00:01 -0500

    preparation for heroku deployment

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") { 
+    port = 3000; 
+};
+app.listen(port, () => console.log(`Server started at port ${port}.`));
